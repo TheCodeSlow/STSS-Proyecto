7ddfa33f5fe7aaeb5018bdcdc175f15fa33a9224
TheCodeSlow <88018660+TheCodeSlow@users.noreply.github.com>
2021-07-29 22:03:39 -0600

    Adicion de Framework ReactJS

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 const db = require('./models');
 
-//const router = require('./m');
 
 
 const postRouter = require('./routes/Posts');
 app.use("/usuarios", postRouter);
+
 
 db.sequelize.sync().then(() =>{
     app.listen(3000, () => {
